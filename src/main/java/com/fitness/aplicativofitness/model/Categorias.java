2f437167629f9290d2de137507e7f0e6e93aed75
Yasminpontess <161251647+Yasminpontess@users.noreply.github.com>
2025-01-21 15:10:45 -0300

    Relacionamento Categorias

--- src/main/java/com/fitness/aplicativofitness/model/Categorias.java
+++ src/main/java/com/fitness/aplicativofitness/model/Categorias.java
@@ -1,9 +1,16 @@
 package com.fitness.aplicativofitness.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
@@ -17,6 +24,18 @@ public class Categorias {
 	
 	@NotNull(message = "O Atributo Descrição é obrigatorio") //NÃO DEIXA O CAMPO SER NULO
 	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categorias", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categorias")
+	private List<Postagem> postagem;
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
 
 	public Long getId() {
 		return id;
