e8199dda32a7c6ad653ae23405556abb9f9be529
Yasminpontess <161251647+Yasminpontess@users.noreply.github.com>
2025-01-21 15:15:38 -0300

    Atualização categorias

--- src/main/java/com/fitness/aplicativofitness/model/Categorias.java
+++ src/main/java/com/fitness/aplicativofitness/model/Categorias.java
@@ -15,7 +15,7 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-@Table(name = "tb_temas")
+@Table(name = "tb_categorias")
 public class Categorias {
 	
 	@Id //CHAVE PRIVARIA 
